b57c389acf7de84fdc24c932fa8c4fdeae2d9744
Andre Matos <andreb17@rede.ulbra.br>
2022-04-29 09:17:44 -0300

    Redirecionando rota com decorator Redirect

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Redirect } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
     constructor(private userService: UserService) { }
     @Get()
+    @Redirect('http://localhost:3000/user/new', 302)
     getUser() {
-        return this.userService.getUser();
+        return '';
+    }
+
+    @Get('new')
+    getUserV2() {
+        return 'Redirected successfully!';
+    }
+
+    @Post()
+    saveUser() {
+        return 'Created';
     }
 }
+
